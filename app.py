5c01521abaa9586cac03810363ae82104856bdbc
SHIN <72782380+SHIN1501020@users.noreply.github.com>
2023-08-10 20:48:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
 from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 
@@ -77,37 +75,6 @@ def login_user():
 def home():
     return render_template('index.html')
 
-# @app.route("/movie", methods=["POST"])
-# def movie_post():
-
-#     sample_url = request.form['sample_url']
-#     sample_comment = request.form['sample_comment']
-#     sample_star = request.form['sample_star']
-
-#     URL = sample_url
-#     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get(URL,headers=headers)
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#     ogtitle = soup.select_one('meta[property="og:title"]')['content']
-#     ogimage = soup.select_one('meta[property="og:image"]')['content']
-#     ogdesc = soup.select_one('meta[property="og:description"]')['content']
-
-#     moive = {
-#         'title' : ogtitle,
-#         'image' : ogimage,
-#         'description' : ogdesc,
-#         'comment' : sample_comment,
-#         'star' : sample_star
-#     }
-#     db.movies2.insert_one(moive)
-
-
-#     return jsonify({'msg':'저장 완료!'})
-
-# @app.route("/movie", methods=["GET"])
-# def movie_get():
-#     all_moives = list(db.movies2.find({},{'_id':False}))
-#     return jsonify({'result': all_moives })
 
 @app.route("/api/reviews", methods=["GET"])
 def reviews_get():
